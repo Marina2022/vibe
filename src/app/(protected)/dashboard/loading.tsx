3d5ee727715e9321c414144c993e974bc3ba569f
Marina2022 <marusiiiia@yandex.ru>
2025-10-28 15:52:56 +0100

    fix

--- src/app/(protected)/dashboard/loading.tsx
+++ src/app/(protected)/dashboard/loading.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+function Minispinner() {
+  return null;
+}
+
 const Loading = () => {
   return (
-    <div>
-      Loading...
+    <div style={{ height: "100%", width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <Minispinner />
     </div>
   );
 };
